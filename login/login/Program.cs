58a7cb59d74c0b7befea556c5978d169f979aa94
Akshaya <akshayaabhilash825@gmail.com>
2026-09-15 11:43:57 +0530

    Add login, operators, and registration projects
    Added solution and project files for login, operators, and registration. Implemented basic login validation in login, "Hello, World!" output in operators, and course registration checker with input validation in registration.

---
+++ login/login/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.Design;
+namespace Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Username:");
+            string Username = Console.ReadLine();
+            Console.WriteLine("Enter the Password:");
+            string Password = Console.ReadLine();
+            if (Password.Length < 8)
+            {
+                Console.WriteLine("Error:Password must be at least 8 characters long");
+            }
+            else if (Password.ToLower().Contains(Username.ToLower()))
+            {
+                Console.WriteLine("Error:Password  should not contain the username");
+            }
+            else
+            {
+                Console.WriteLine("Login Succesful!");
+            }
+        }
+    }
+}
+
---
+++ registration/registration/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class CourseRegistrationChecker
+{
+    static void Main(string[]args)
+      
+    {
+        Console.Write("Enter your full name: ");
+        string fullName = Console.ReadLine();
+
+        Console.Write("Enter the course : ");
+        string courseName = Console.ReadLine();
+
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        Console.Write("Have you completed the prerequisites? (yes/no): ");
+        string prerequisites = Console.ReadLine();
+
+        
+        Console.WriteLine($"\nHello {fullName.ToUpper()}, welcome to the registration portal!");
+
+        
+        if (courseName.IndexOf("C#", StringComparison.OrdinalIgnoreCase) >= 0)
+        {
+            Console.WriteLine("The course you selected includes C#.");
+        }
+        else
+        {
+            Console.WriteLine(" The course you selected does not include C#.");
+        }
+
+       
+        if (age >= 18 && prerequisites.Equals("yes", StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine(" Registration successful! You meet all the requirements.");
+        }
+        else
+        {
+            Console.WriteLine(" Registration failed.");
+            if (age < 18)
+            {
+                Console.WriteLine("- You must be at least 18 years old.");
+            }
+            if (!prerequisites.Equals("yes", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("- You must complete the prerequisites before registering.");
+            }
+        }
+    }
+}
+
