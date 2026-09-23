0629c99dbc06852606f4090ba78dec70c23439d2
Akshaya <akshayaabhilash825@gmail.com>
2026-09-23 12:01:49 +0530

    current date and time

---
+++ Current Date/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            DateTime CurrentDate = DateTime.Now;
+            DateTime FutureDate = CurrentDate.AddDays(5);
+            TimeSpan Difference = FutureDate - CurrentDate;
+            int DaysLeft = Difference.Days;
+
+
+            string Result = $"Current Date:{CurrentDate},Future Date:{FutureDate},Days Left:{DaysLeft}";
+            Console.WriteLine(Result);
+            string filePath = "date_result.txt";
+            File.WriteAllText(filePath, Result);
+
+            Console.WriteLine("Calculation successful! Result saved to date_result.txt.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred:");
+        }
+    }
+}
